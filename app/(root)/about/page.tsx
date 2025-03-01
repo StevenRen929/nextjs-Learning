8549eea58404467192553bf045534e860887b3fd
Steven Ren <stevenfrog@outlook.com>
2025-03-01 22:32:17 +1030

    error handling

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const page = () => {
+  throw new Error ("message:not implemented")
   return (
     <div className='text-2xl'>ABOUT:   This is about nextjs learning</div>
   )
