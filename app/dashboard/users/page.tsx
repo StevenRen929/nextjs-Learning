21424394d6140481f6d2aa3503b005782f4199d6
Steven Ren <stevenfrog@outlook.com>
2025-03-01 20:44:47 +1030

    folder-base-routing basic

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Dash board user </h1>
+            <ul className='mt-10'>
+                    <li>user  1</li>
+                    <li>user  2</li>
+                    <li>user  3</li>
+                    <li>user  4</li>
+            </ul>
+       
+    </div>
+  )
+}
+
+export default page
